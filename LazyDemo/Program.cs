4ace757ad4be1d49cdc5d4abd7dfae98f4e0604a
SingletonSean <singletonsean4@gmail.com>
2021-01-24 11:46:03 -0500

    Add console lazy demo

--- LazyDemo/Program.cs
+++ LazyDemo/Program.cs
@@ -9,19 +9,56 @@ namespace LazyDemo
     {
         static async Task Main(string[] args)
         {
-            Task<Account> accountTask = CreateAccount();
-            Lazy<Task<Account>> getAccount = new Lazy<Task<Account>>(CreateAccount);
-
-            Account account1 = await accountTask;
-            Account account2 = await accountTask;
-
-            Console.WriteLine(account1.Id);
-            Console.WriteLine(account2.Id);
-
+            Console.WriteLine("Press Enter to login.");
             Console.ReadLine();
+
+            Account account = await GetAccount();
+
+            Console.WriteLine("Successfully logged in.");
+
+            while(true)
+            {
+                Console.WriteLine("Enter the number corresponding to the desired action:");
+                Console.WriteLine("1. Show Account Details");
+                Console.WriteLine("2. Show Balance");
+                Console.WriteLine("3. Show Daily Message");
+                Console.WriteLine("4. Exit");
+
+                if(int.TryParse(Console.ReadLine(), out int input) && input >= 1 && input <= 4)
+                {
+                    if(input == 1)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Email: {account.Email}");
+                        Console.WriteLine($"Username: {account.Username}");
+                        Console.WriteLine();
+                    }
+                    else if(input == 2)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Balance: {account.Balance:C}");
+                        Console.WriteLine();
+                    }
+                    else if (input == 3)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Daily Message: Hello world.");
+                        Console.WriteLine();
+                    }
+                    else if (input == 4)
+                    {
+                        Console.WriteLine("Exiting...");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
         }
 
-        private static async Task<Account> CreateAccount()
+        private static async Task<Account> GetAccount()
         {
             Account account = new Account
             {
