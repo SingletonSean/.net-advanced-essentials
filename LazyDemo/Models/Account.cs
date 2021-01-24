fb2e269bf5202810789bda0ba72ac40438c27170
SingletonSean <singletonsean4@gmail.com>
2021-01-24 11:13:51 -0500

    Add lazy quick demo

---
+++ LazyDemo/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazyDemo.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public double Balance { get; set; }
+    }
+}
---
+++ LazyDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using LazyDemo.Models;
+using System;
+using System.Threading.Tasks;
+
+
+namespace LazyDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task<Account> accountTask = CreateAccount();
+            Lazy<Task<Account>> getAccount = new Lazy<Task<Account>>(CreateAccount);
+
+            Account account1 = await accountTask;
+            Account account2 = await accountTask;
+
+            Console.WriteLine(account1.Id);
+            Console.WriteLine(account2.Id);
+
+            Console.ReadLine();
+        }
+
+        private static async Task<Account> CreateAccount()
+        {
+            Account account = new Account
+            {
+                Id = Guid.NewGuid(),
+                Email = "test@gmail.com",
+                Username = "test",
+                Balance = 500
+            };
+
+            await Task.Delay(3000);
+
+            return account;
+        }
+    }
+}
