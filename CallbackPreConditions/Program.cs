fd4a5c9d140b6b402bee8aab8f101d1645e8f3a7
SingletonSean <singletonsean4@gmail.com>
2023-06-06 19:40:53 -0400

    Setup callback precondition demo project

---
+++ CallbackPreConditions/Program.cs
@@ -0,0 +1,21 @@
+﻿using CallbackPreConditions;
+
+UserCache userCache = new UserCache("./user-cache.json");
+
+Console.WriteLine("Getting cached user");
+User? cachedUser = await userCache.Get();
+Console.WriteLine($"Cached user: {cachedUser?.Username} {cachedUser?.Website}");
+
+Console.WriteLine("Setting cached user");
+await userCache.Set(new User()
+{
+    Username = "SingletonSean",
+    Website = "https://seandodson.com"
+});
+
+Console.WriteLine("Getting cached user");
+User? updatedUser = await userCache.Get();
+Console.WriteLine($"Cached user: {updatedUser?.Username} {updatedUser?.Website}");
+
+Console.WriteLine("Clearing cached user");
+await userCache.Clear();
---
+++ CallbackPreConditions/UserCache.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CallbackPreConditions
+{
+    public class UserCache
+    {
+        private readonly string _fileCachePath;
+
+        public UserCache(string fileCachePath)
+        {
+            _fileCachePath = fileCachePath;
+        }
+
+        public async Task<User?> Get()
+        {
+            if (!File.Exists(_fileCachePath))
+            {
+                return null;
+            }
+
+            string userJson = await File.ReadAllTextAsync(_fileCachePath);
+
+            if (userJson.Length == 0)
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<User>(userJson);
+        }
+
+        public async Task Set(User user)
+        {
+            await File.WriteAllTextAsync(_fileCachePath, JsonSerializer.Serialize(user));
+        }
+
+        public async Task Clear()
+        {
+            await File.WriteAllTextAsync(_fileCachePath, string.Empty);
+        }
+
+        private bool HasFileCachePermission()
+        {
+            try
+            {
+                using FileStream fs = File.Open(_fileCachePath, FileMode.OpenOrCreate);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
