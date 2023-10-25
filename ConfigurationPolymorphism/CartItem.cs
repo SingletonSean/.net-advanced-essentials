a1c9b8d7543de46229298d63f5ddd3513da06827
SingletonSean <singletonsean4@gmail.com>
2023-10-24 20:15:31 -0400

    Add initial polymorphism demo

---
+++ ConfigurationPolymorphism/CartItem.cs
@@ -0,0 +1,51 @@
+﻿namespace ConfigurationPolymorphism
+{
+    public class CartItem
+    {
+        private const decimal WARRANTY_PRICE = 10.00m;
+        private const int BASE_WARRANTY_DAYS  = 30;
+        private const int EXTENDED_WARRANTY_ADDITIONAL_DAYS  = 90;
+
+        private readonly decimal _basePrice;
+        private readonly bool _hasExtendedWarranty;
+
+        public string Name { get; set; }
+
+        public decimal Price
+        {
+            get
+            {
+                decimal calculatedPrice = _basePrice;
+
+                if (_hasExtendedWarranty)
+                {
+                    calculatedPrice += WARRANTY_PRICE;
+                }
+
+                return calculatedPrice;
+            }
+        }
+
+        public int WarrantyDays
+        {
+            get
+            {
+                int calculatedWarrantyDays = BASE_WARRANTY_DAYS;
+
+                if (_hasExtendedWarranty)
+                {
+                    calculatedWarrantyDays += EXTENDED_WARRANTY_ADDITIONAL_DAYS;
+                }
+
+                return calculatedWarrantyDays;
+            }
+        }
+
+        public CartItem(string name, decimal basePrice, bool hasExtendedWarranty)
+        {
+            Name = name;
+            _basePrice = basePrice;
+            _hasExtendedWarranty = hasExtendedWarranty;
+        }
+    }
+}
---
+++ ConfigurationPolymorphism/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConfigurationPolymorphism;
+
+CartItem cartItem = new CartItem("Skateboard", 99.99m, false);
+
+Console.WriteLine($"{cartItem.Name} | ${cartItem.Price} | {cartItem.WarrantyDays} day warranty");
